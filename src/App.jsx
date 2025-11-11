6590fce94ad21dd92cad61b9dd1f6f7f90d45831
raees-aziz <raees4963@gmail.com>
2025-11-12 00:34:01 +0500

    make a custom pointer

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,59 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect, useRef } from "react";
+import gsap from "gsap";
+// import Cursor from "./components/Cursor";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+
+  const scaleIn=()=>{
+    gsap.to("#cursor",{
+      background:'cyan',
+      color:'#ff0000',
+      scale:8,
+      duration:0.3,
+    })
+    // h1 text
+    gsap.to(".text",{
+      color:'#ff0000',
+    })
+  }
+  const scaleOut=()=>{
+    gsap.to("#cursor",{
+      background:'#05df72 ',
+      color:'white',
+      scale:1,
+      duration:0.3,
+    })
+    // h1 text
+    gsap.to(".text",{
+      color:'white',
+    })
+  }
+
+  useEffect(() => {
+    const moveCursor = (e) => {
+      const { clientX, clientY } = e;
+      gsap.to("#cursor", {
+        x: clientX,
+        y: clientY,
+        duration: 0.2,
+        ease: "power2.out",
+      });
+    };
+
+    window.addEventListener("mousemove", moveCursor);
+    return () => window.removeEventListener("mousemove", moveCursor);
+  }, []);
+
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="h-screen w-full bg-black cursor-none flex items-center justify-center">
+      <div
+      id='cursor'
+      className="size-5  rounded-full bg-green-400 fixed top-0 left-0 mix-blend-difference pointer-events-none z-[9]"
+    />
+      <h1 className="text-8xl text text-white z-100" onMouseLeave={scaleOut} onMouseEnter={scaleIn}>Hover Me!</h1>
+    </div>
+  );
+};
 
-export default App
+export default App;
